1fc1247d897fba1a5d7c944e3394030e5724589d
vagrant <vagrant@localhost.localdomain>
2020-04-12 06:32:31 +0000

    [book-model]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root 'homes#top'
+  root to: 'homes#top'
   get 'homes/about', 'homes#about'
 
   devise_for :users
-  resources :books, only:[:index, :edit, :create, :update]
-  resources :users, only:[:index, :edit, :create, :update, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    authenticated :user do
+      root to: 'uses#show', as: :user_root
+      resources :books, only:[:index, :edit, :create, :update]
+      resources :users, only:[:index, :edit, :create, :update, :show]
+    end
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
